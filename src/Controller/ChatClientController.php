f1b30fae0cccd2d4fc77bbe1d12dfe96fd95cac1
Marvin Beym <mBeym@databay.de>
2022-10-24 11:34:02 +0200

    Improve api usage, add checks, add encrypted info

--- src/Controller/ChatClientController.php
+++ src/Controller/ChatClientController.php
@@ -19,22 +19,19 @@ namespace ILIAS\Plugin\MatrixChatClient\Controller;
 use ilTemplate;
 use ILIAS\DI\Container;
 use ILIAS\Plugin\MatrixChatClient\Model\CourseSettings;
-use ILIAS\Plugin\MatrixChatClient\Api\MatrixApiCommunicator;
 use ilUtil;
 use ilObjCourseGUI;
-use ILIAS\Plugin\MatrixChatClient\Model\Room\RoomInvite;
-use ilMatrixChatClientUIHookGUI;
 use ilTemplateException;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Message\RequestInterface;
-use Defuse\Crypto\Crypto;
-use ilUIPluginRouterGUI;
-use ilCourseBookingUIHookGUI;
 use ILIAS\Filesystem\Stream\Streams;
 use ILIAS\Plugin\MatrixChatClient\Repository\CourseSettingsRepository;
+use Exception;
+use ilCourseParticipants;
 
 /**
  * Class ChatClientController
+ *
  * @package ILIAS\Plugin\MatrixChatClient\Controller
  * @author  Marvin Beym <mbeym@databay.de>
  */
@@ -80,6 +77,21 @@ class ChatClientController extends BaseController
         $this->mainTpl->loadStandardTemplate();
 
         $this->ctrl->setParameterByClass(ilObjCourseGUI::class, "ref_id", $this->courseId);
+        if (!$this->courseSettings || !$this->courseSettings->isChatIntegrationEnabled()) {
+            $this->ctrl->redirectByClass(
+                ["ilRepositoryGUI", "ilObjCourseGUI"],
+                "view"
+            );
+        }
+
+        if (!ilCourseParticipants::_isParticipant($this->courseId, $this->dic->user()->getId())) {
+            ilUtil::sendFailure($this->lng->txt("permission_denied", true));
+            $this->ctrl->redirectByClass(
+                ["ilRepositoryGUI", "ilObjCourseGUI"],
+                "view"
+            );
+        }
+
         $this->tabs->setBackTarget(
             $this->lng->txt("content"),
             $this->ctrl->getLinkTargetByClass(
@@ -88,42 +100,53 @@ class ChatClientController extends BaseController
             )
         );
 
-        if (($matrixUser = $this->matrixApi->user->login("admin", "Test123")) === null) {
-            ilUtil::sendFailure($this->plugin->txt("matrix.chat.login.failed"), true);
+        try {
+            if (($matrixUser = $this->matrixApi->user->login($this->dic->user()->getId())) === null) {
+                ilUtil::sendFailure($this->plugin->txt("matrix.chat.login.failed"), true);
+                $this->mainTpl->printToStdOut();
+                return;
+            }
+        } catch (Exception $e) {
+            ilUtil::sendFailure($this->plugin->txt($e->getMessage()), true);
             $this->mainTpl->printToStdOut();
             return;
         }
 
-        $room = $this->matrixApi->user->getRoomByCourseId($this->courseId);
+        $room = $this->matrixApi->admin->getRoom($this->courseSettings->getMatrixRoomId());
 
         if (!$room) {
-            ilUtil::sendFailure($this->plugin->txt("chat_client_integration_room_not_found"), true);
+            ilUtil::sendFailure($this->plugin->txt("matrix.chat.room.notFound"), true);
             $this->mainTpl->printToStdOut();
             return;
         }
 
-        if ($room instanceof RoomInvite) {
-            ilUtil::sendInfo($this->plugin->txt("chat_client_integration_room_invite_available"), true);
-            $this->ctrl->setParameterByClass(ilMatrixChatClientUIHookGUI::class, "ref_id", $this->courseId);
-            $joinButton = $this->dic->ui()->factory()->button()->standard(
-                $this->lng->txt("join"),
-                $this->getCommandLink("joinRoom")
-            );
-            $this->mainTpl->setContent($this->dic->ui()->renderer()->render($joinButton));
-            $this->mainTpl->printToStdOut();
-            return;
+        if (
+            !$this->matrixApi->admin->isUserMemberOfRoom($matrixUser, $this->courseSettings->getMatrixRoomId())
+        ) {
+            if (!$this->matrixApi->admin->addUserToRoom($matrixUser, $room)) {
+                ilUtil::sendFailure($this->plugin->txt("matrix.chat.room.memberAssignFailed"), true);
+                $this->ctrl->redirectByClass(
+                    ["ilRepositoryGUI", "ilObjCourseGUI"],
+                    "view"
+                );
+            }
         }
-        $message = $room->getTimeline()["events"][array_key_last($room->getTimeline()["events"])];
-        $data = $message["content"];
 
-        //$a = openssl_decrypt($data["ciphertext"], "aes-256-cbc-hmac-sha1", "ZgmQiaSHT4UrK6GP3JNsy48FJCAT/SfPqFJ4k/GmfFg");
 
+        $this->mainTpl->addJavaScript($this->plugin->jsFolder("libs/easymde.min.js"));
+        $this->mainTpl->addJavaScript($this->plugin->jsFolder("libs/markdown-it.min.js"));
         $this->mainTpl->addJavaScript($this->plugin->jsFolder("libs/olm.js"));
         $this->mainTpl->addJavaScript($this->plugin->jsFolder("libs/browser-matrix.js"));
         $this->mainTpl->addJavaScript($this->plugin->jsFolder("test.js"));
-        $this->mainTpl->addJavaScript($this->plugin->jsFolder("test.js"));
         $this->mainTpl->addCss($this->plugin->cssFolder("style.css"));
-        $tpl = new ilTemplate($this->plugin->templatesFolder("tpl.chat-integration.html"), false, false);
+        $this->mainTpl->addCss($this->plugin->cssFolder("easymde.min.css"));
+
+        $tpl = new ilTemplate($this->plugin->templatesFolder("tpl.chat-integration.html"), true, true);
+        $tpl->setVariable(
+            $room->isEncrypted() ? "ENCRYPTED_TEXT" : "UNENCRYPTED_TEXT",
+            $this->plugin->txt($room->isEncrypted() ? "matrix.chat.room.encryption.encrypted" : "matrix.chat.room.encryption.unencrypted")
+        );
+        $tpl->setVariable("SEND_TEXT", $this->plugin->txt("matrix.chat.send"));
         $this->mainTpl->addOnLoadCode(
             "window.matrixChatConfig = " . json_encode([
                 "baseUrl" => "http://localhost:8008",
@@ -133,8 +156,22 @@ class ChatClientController extends BaseController
                     ]),
                 ],
                 "user" => $matrixUser,
-                "roomId" => $room->getRoomId(),
+                "roomId" => $room->getId(),
             ], JSON_THROW_ON_ERROR)
+        );
+
+        $translationFilePath = "{$this->plugin->getDirectory()}/lang/{$this->lng->getLangKey()}.lang.json";
+        $translationJson = "{}";
+        if (is_file($translationFilePath) && is_readable($translationFilePath)) {
+            $translationJson = file_get_contents($translationFilePath);
+        } else {
+        }
+
+        $this->mainTpl->addOnLoadCode(
+            "window.matrixChatTranslation = " . json_encode(
+                $translationJson,
+                JSON_THROW_ON_ERROR
+            )
         );
 
 
@@ -164,25 +201,5 @@ class ChatClientController extends BaseController
         $http->saveResponse($http->response()->withBody($responseStream));
         $http->sendResponse();
         $http->close();
-    }
-
-    public function joinRoom() : void
-    {
-        //ToDo: Replace manual login with getting username & password from user field
-        //ToDo: Also move logging in to api calls by checking if accessToken is set, if not => login
-        if (!$this->matrixApi->user->login("user1", "Test123")) {
-            ilUtil::sendFailure($this->plugin->txt("chat_client_integration_login_failed"), true);
-            $this->redirectToCommand("showChat", ["ref_id" => $this->courseId]);
-            return;
-        }
-        $room = $this->matrixApi->user->getRoomByCourseId($this->courseId);
-
-        if (!$room instanceof RoomInvite) {
-            ilUtil::sendFailure($this->plugin->txt("chat_client_integration_room_invite_invalid"), true);
-            $this->redirectToCommand("showChat", ["ref_id" => $this->courseId]);
-            return;
-        }
-
-        $this->matrixApi->user->joinRoom($room);
     }
 }
