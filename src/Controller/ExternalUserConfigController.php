5f4bfeec089820195051cc40f7c22da80bcdae55
Marvin Beym <mBeym@databay.de>
2024-01-10 12:57:45 +0100

    Implemenet External user config form

---
+++ src/Controller/ExternalUserConfigController.php
@@ -0,0 +1,156 @@
+<?php
+
+/**
+ * This file is part of ILIAS, a powerful learning management system
+ * published by ILIAS open source e-Learning e.V.
+ *
+ * ILIAS is licensed with the GPL-3.0,
+ * see https://www.gnu.org/licenses/gpl-3.0.en.html
+ * You should have received a copy of said license along with the
+ * source code, too.
+ *
+ * If this is not the case or you just want to try ILIAS, you'll find
+ * us at:
+ * https://www.ilias.de
+ * https://github.com/ILIAS-eLearning
+ *
+ *********************************************************************/
+
+declare(strict_types=1);
+
+
+namespace ILIAS\Plugin\MatrixChatClient\Controller;
+
+use ILIAS\Plugin\MatrixChatClient\Form\BaseUserConfigForm;
+use ILIAS\Plugin\MatrixChatClient\Form\ExternalUserConfigForm;
+use ILIAS\Plugin\MatrixChatClient\Form\PluginConfigForm;
+
+/**
+ * Class ExternalUserConfigController
+ *
+ * @package ILIAS\Plugin\MatrixChatClient\Controller
+ * @author  Marvin Beym <mbeym@databay.de>
+ */
+class ExternalUserConfigController extends BaseUserConfigController
+{
+    public function showUserChatConfig(?BaseUserConfigForm $form = null): void
+    {
+        $this->injectTabs(self::TAB_USER_CHAT_CONFIG);
+        $this->mainTpl->loadStandardTemplate();
+
+        if (!$form) {
+            $form = new ExternalUserConfigForm(
+                $this,
+                $this->user,
+                $this->userConfig->getMatrixUserId(),
+                $this->userConfig->getAuthMethod(),
+                !$this->matrixApi->admin->userExists($this->buildMatrixUserId())
+            );
+
+            $form->setValuesByArray(array_merge(
+                $this->userConfig->toArray(),
+                [
+                    "connectedHomeserver" => $this->plugin->getPluginConfig()->getMatrixServerUrl(),
+                    "matrixUsername" => $this->buildMatrixUserId(),
+                    "matrixAccount" => $this->userConfig->getMatrixUserId()
+                ]
+            ), true);
+        }
+
+        $this->renderToMainTemplate($form->getHTML());
+    }
+
+    public function saveUserChatConfig(): void
+    {
+        $form = new ExternalUserConfigForm(
+            $this,
+            $this->user,
+            null,
+            null,
+            !$this->matrixApi->admin->userExists($this->buildMatrixUserId())
+        );
+
+        if (!$form->checkInput()) {
+            $form->setValuesByPost();
+            $this->showUserChatConfig($form);
+            return;
+        }
+
+        $form->setValuesByPost();
+
+        $authMethod = $form->getInput("authMethod");
+        $matrixUserPassword = $form->getInput("matrixUserPassword");
+        $matrixUserId = $this->buildMatrixUserId();
+        $this->userConfig->setAuthMethod($form->getInput("authMethod"));
+
+        if ($authMethod === PluginConfigForm::CREATE_ON_CONFIGURED_HOMESERVER) {
+            if ($this->matrixApi->admin->userExists($matrixUserId)) {
+                $matrixUser = $this->matrixApi->admin->loginUserWithAdmin($this->user->getId(), $matrixUserId);
+
+                if (!$matrixUser) {
+                    //Logging into user failed, even though it exists (should never happen)
+                    $this->uiUtil->sendFailure($this->plugin->txt("config.user.auth.failure"), true);
+                    $this->redirectToCommand(self::CMD_SHOW_USER_CHAT_CONFIG);
+                }
+
+                $this->userConfig
+                    ->setMatrixUserId($matrixUser->getMatrixUserId())
+                    ->save();
+
+                $this->uiUtil->sendSuccess($this->plugin->txt("config.user.auth.success"), true);
+                $this->redirectToCommand(self::CMD_SHOW_USER_CHAT_CONFIG);
+            } else {
+                //Register new account
+                $username = $this->buildUsername();
+
+                if (!$this->matrixApi->admin->usernameAvailable($username)) {
+                    //Should only ever happen if a user was register in the same moment this code ran (unlikely)
+                    $this->uiUtil->sendFailure($this->plugin->txt("config.user.register.failure.usernameAlreadyUsed"));
+                    $this->redirectToCommand(self::CMD_SHOW_USER_CHAT_CONFIG);
+                }
+
+                if (!$matrixUserPassword) {
+                    //Missing password (should never happen as form check should already fail before
+                    $this->uiUtil->sendFailure($this->plugin->txt("config.user.register.failure"), true);
+                    $this->redirectToCommand(self::CMD_SHOW_USER_CHAT_CONFIG);
+                }
+
+                $matrixUser = $this->matrixApi->admin->createUser(
+                    $username,
+                    $matrixUserPassword,
+                    $this->user->getFullname()
+                );
+
+                if (!$matrixUser) {
+                    //Creation failed, unknown cause.
+                    $this->uiUtil->sendFailure($this->plugin->txt("config.user.register.failure"), true);
+                    $this->redirectToCommand(self::CMD_SHOW_USER_CHAT_CONFIG);
+                }
+
+                $this->userConfig
+                    ->setMatrixUserId($matrixUser->getMatrixUserId())
+                    ->save();
+
+                $this->uiUtil->sendSuccess($this->plugin->txt("config.user.register.success"), true);
+                $this->redirectToCommand(self::CMD_SHOW_USER_CHAT_CONFIG);
+            }
+        } else {
+        }
+    }
+
+    public function buildUsername(): string
+    {
+        $username = $this->plugin->getPluginConfig()->getExternalUserScheme();
+        foreach ($this->plugin->getUsernameSchemeVariables() as $key => $value) {
+            $username = str_replace("{" . $key . "}", $value, $username);
+        }
+        return $username;
+    }
+
+    protected function buildMatrixUserId(): string
+    {
+        $url = parse_url($this->plugin->getPluginConfig()->getMatrixServerUrl());
+
+        return "@{$this->buildUsername()}:{$url["host"]}";
+    }
+}
---
+++ src/Form/ExternalUserConfigForm.php
@@ -0,0 +1,116 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * This file is part of ILIAS, a powerful learning management system
+ * published by ILIAS open source e-Learning e.V.
+ *
+ * ILIAS is licensed with the GPL-3.0,
+ * see https://www.gnu.org/licenses/gpl-3.0.en.html
+ * You should have received a copy of said license along with the
+ * source code, too.
+ *
+ * If this is not the case or you just want to try ILIAS, you'll find
+ * us at:
+ * https://www.ilias.de
+ * https://github.com/ILIAS-eLearning
+ *
+ *********************************************************************/
+
+namespace ILIAS\Plugin\MatrixChatClient\Form;
+
+use ILIAS\Plugin\MatrixChatClient\Controller\BaseUserConfigController;
+use ILIAS\Plugin\MatrixChatClient\Controller\ExternalUserConfigController;
+use ilObjUser;
+use ilPasswordInputGUI;
+use ilRadioOption;
+
+/**
+ * Class ExternalUserConfigForm
+ *
+ * @package ILIAS\Plugin\MatrixChatClient\Form
+ * @author  Marvin Beym <mbeym@databay.de>
+ */
+class ExternalUserConfigForm extends BaseUserConfigForm
+{
+    private bool $passwordEnterNeeded;
+
+    public function __construct(
+        ExternalUserConfigController $controller,
+        ilObjUser $user,
+        ?string $matrixAccountId = null,
+        ?string $selectedAccountOption = null,
+        bool $passwordEnterNeeded = false
+    ) {
+        $this->passwordEnterNeeded = $passwordEnterNeeded;
+        parent::__construct($controller, $user, $matrixAccountId, $selectedAccountOption);
+    }
+
+    protected function showCommandButton(bool $reset = false): void
+    {
+        if ($reset) {
+            $this->addCommandButton(
+                ExternalUserConfigController::getCommand(BaseUserConfigController::CMD_RESET_ACCOUNT_SETTINGS),
+                $this->plugin->txt("config.user.resetAccountSettings")
+            );
+        } else {
+            $this->addCommandButton(ExternalUserConfigController::getCommand(
+                BaseUserConfigController::CMD_SAVE_USER_CHAT_CONFIG),
+                $this->lng->txt("save")
+            );
+        }
+    }
+
+
+    public function getCreateOnConfiguredHomeserverOption(): ilRadioOption
+    {
+        $radioOption = new ilRadioOption(
+            $this->plugin->txt("config.user.method.createOnConfiguredHomeserver"),
+            "createOnConfiguredHomeserver"
+        );
+
+
+        $radioOption->addSubItem($this->getConnectedMatrixHomeserverInput());
+        $radioOption->addSubItem($this->getMatrixAccountInput(
+            "config.user.method.createOnConfiguredHomeserver.detectedMatrixAccount",
+            "matrixUsername",
+            true
+        ));
+
+        if ($this->passwordEnterNeeded) {
+            $matrixUserPassword = new ilPasswordInputGUI(
+                $this->plugin->txt("matrix.user.password"),
+                "matrixUserPassword"
+            );
+            $matrixUserPassword->setInfo($this->plugin->txt("matrix.user.password.external.required"));
+            $matrixUserPassword->setRequired(true);
+
+            $radioOption->addSubItem($matrixUserPassword);
+        }
+
+        return $radioOption;
+    }
+
+    public function getSpecifyOtherMatrixAccountOption(): ilRadioOption
+    {
+        $radioOption = new ilRadioOption(
+            $this->plugin->txt("config.user.method.specifyOtherMatrixAccount"),
+            "specifyOtherMatrixAccount"
+        );
+
+        $radioOption->addSubItem($this->getMatrixAccountInput(
+            "config.user.method.specifyOtherMatrixAccount.accountName",
+            "matrixAccount"
+        ));
+        return $radioOption;
+    }
+
+    protected function onAuthenticated(): bool
+    {
+        $this->addItem($this->getConnectedMatrixHomeserverInput());
+        $this->addItem($this->getMatrixAccountInput("matrix.user.account", "matrixAccount", true));
+
+        $this->showCommandButton(true);
+        return false;
+    }
+}
